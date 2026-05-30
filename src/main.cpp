85b87d712587bfa03c1163230697f49b031892db
gorurarda561-dot <gorurarda561@gmail.com>
2026-05-30 13:46:42 +0300

    while döngüsü eklendi

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
+bool arda = true;
 int main (int argc, char *argv[]) {
     cout << "Toplam argüman sayısı:" << argc << endl;
+    cout << "toplam kelime sayısı:" << *argv << endl;
+    int argüman = 0;
+    while (argüman<argc) {
+        cout << "argüman" << argüman+1 << ":" << argv[argüman] << endl;
+        argüman++;
+    }
     return 0;
+
 }
